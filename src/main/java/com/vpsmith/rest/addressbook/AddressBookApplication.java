ac481987091be8932d60a5279b92259b5ed4e4f8
vpsmith54 <vpsmith54@gmail.com>
2019-10-20 17:26:01 -0700

    convert to use undertow httpserver

---
+++ src/main/java/com/vpsmith/rest/addressbook/AddressBookApplication.java
@@ -0,0 +1,12 @@
+package com.vpsmith.rest.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApplication.class, args);
+    }
+}
--- src/main/java/com/vpsmith/rest/addressbook/contact/ContactRepository.java
+++ src/main/java/com/vpsmith/rest/addressbook/contact/ContactRepository.java
@@ -1,7 +1,9 @@
 package com.vpsmith.rest.addressbook.contact;
 
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ContactRepository extends CrudRepository<ContactEntity, Integer> {
-
+@EnableScan
+public interface ContactRepository extends
+        CrudRepository<ContactEntity, Integer> {
 }
